8609d778d1cbe1704fd272411e3cb9ce82e30b3e
Junior Souza <fabriciojuniorsc@gmail.com>
2022-02-05 16:38:35 -0300

    adicionada dependência do Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'agenda-escolar-363c6',
+    appId: '1:854701707382:web:5654506818d40e11f50f57',
+    storageBucket: 'agenda-escolar-363c6.appspot.com',
+    apiKey: 'AIzaSyCPxscu_p7VSwuxFhftvCJtjinNEIvlu3k',
+    authDomain: 'agenda-escolar-363c6.firebaseapp.com',
+    messagingSenderId: '854701707382',
+    measurementId: 'G-1GY9FB61V5',
+  },
   production: false
 };
 
