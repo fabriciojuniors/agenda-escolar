2c236872e111cb25da4fc2bc8d99159f5150f768
Junior Souza <fabriciojuniorsc@gmail.com>
2022-02-05 14:02:47 -0300

    Configurações iniciais

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.agenda-escolar',
   appName: 'agenda-escolar',
   webDir: 'www',
   bundledWebRuntime: false
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Agenda', url: '/', icon: 'calendar-number-outline' },
+    { title: 'Horário Escolar', url: '/2', icon: 'time-outline' },
+    { title: 'Perfil', url: '/2', icon: 'person-outline' },
+    { title: 'Preferências', url: '/2', icon: 'cog-outline' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
