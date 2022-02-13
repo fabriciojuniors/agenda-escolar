b3c0ec4e4dcc697e5d001abe562dd1846448c158
Junior Souza <fabriciojuniorsc@gmail.com>
2022-02-13 17:30:28 -0300

    Ajuste na gravação dos token

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -3,17 +3,25 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 import { Capacitor } from '@capacitor/core';
-import { ActionPerformed, PushNotifications, PushNotificationSchema, Token } from '@capacitor/push-notifications';
+import {
+  ActionPerformed,
+  PushNotifications,
+  PushNotificationSchema,
+  Token,
+} from '@capacitor/push-notifications';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
-
-  constructor(private router: Router, private auth : AngularFireAuth, private db : AngularFirestore) { }
+  constructor(
+    private router: Router,
+    private auth: AngularFireAuth,
+    private db: AngularFirestore
+  ) {}
 
   public initPush() {
-    if (Capacitor.platform !== "web") {
+    if (Capacitor.platform !== 'web') {
       this.registerPush();
     }
   }
@@ -23,43 +31,42 @@ export class NotificationService {
       if (permission.receive === 'granted') {
         PushNotifications.register();
       } else {
-        console.log("Sem acesso");
+        console.log('Sem acesso');
+      }
+    });
 
-      }
-    })
+    PushNotifications.addListener('registration', (token: Token) => {
+      this.auth.authState.subscribe((state) => {
+        let notificationTokenRef = this.db.collection('notification_token').ref;
 
-    PushNotifications.addListener('registration',
-      (token: Token) => {
-        this.auth.authState.subscribe( state => {
-          this.db.collection("notification_token", (ref) => ref.where("user_id", "==", state.uid)).snapshotChanges()
-            .subscribe(data => {
-              data.map(item => {
-                this.db.collection("notification_token").doc(item.payload.doc.id).delete();
-              })
-            }).unsubscribe()
-          this.db.collection("notification_token").add({user_id: state.uid, token: token});
-        }) 
-      }
-    );
-    PushNotifications.addListener('registrationError',
-      (error: any) => {
-        alert('Error on registration: ' + JSON.stringify(error));
-      }
-    );
+        notificationTokenRef
+          .where('user_id', '==', state.uid)
+          .get()
+          .then((data) => {
+            if (data.size == 0) {
+              notificationTokenRef.add({ user_id: state.uid, token: token });
+            }
+          });
+      });
+    });
+    PushNotifications.addListener('registrationError', (error: any) => {
+      alert('Error on registration: ' + JSON.stringify(error));
+    });
 
     // Show us the notification payload if the app is open on our device
-    PushNotifications.addListener('pushNotificationReceived',
+    PushNotifications.addListener(
+      'pushNotificationReceived',
       (notification: PushNotificationSchema) => {
         //alert('Push received: ' + JSON.stringify(notification));
       }
     );
 
     // Method called when tapping on a notification
-    PushNotifications.addListener('pushNotificationActionPerformed',
+    PushNotifications.addListener(
+      'pushNotificationActionPerformed',
       (notification: ActionPerformed) => {
         //alert('Push action performed: ' + JSON.stringify(notification));
       }
     );
   }
-
 }
